1a3a2d3dfcd33e0fe4b95a21a799bb14bc4059fd
Trevorr Marshall <trevorr.marshall@cewe.de>
2025-02-25 08:12:27 +0100

    Added some todos. Turned off app bar color

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,10 @@ void main() {
   );
 }
 
+// TODO: Add a "Favorites" view that shows the user's favorite items (Needs favorites functionality).
+// TODO: MAKE IT SO THAT THE MAIN SCREEN CONTENT IS CENTERED AND NOT STRETCHED OUT. WHEN THE SCREEN IS TOO WIDE.
+// TODO: Make the top bar match the color scheme a bit and change the text color and font.
+// TODO: Add an "Options" menu/ view for dark/light modes and other settings. (maybe sound?)
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -16,9 +20,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Color(0xFFFDD275)),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xFFEE9231),
-        ),
+        // appBarTheme: AppBarTheme(
+        //   backgroundColor: Color(0xFFEE9231),
+        // ),
         bottomNavigationBarTheme: BottomNavigationBarThemeData(
           selectedItemColor: Color(0xFF5B7C3B),
           unselectedItemColor: Color(0xFF5B7C3B).withOpacity(0.5),
